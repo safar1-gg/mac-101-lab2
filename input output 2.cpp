e3bed3fe25109c776cbf40277710e6ef8e6c8bcc
safar1-gg <sagargiri620@gmail.com>
2025-09-25 19:42:21 -0400

    Add files via upload

---
+++ input output 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ string name;
+ string major;
+ string movie;
+ cout<<"enter your name:";
+ cin>> name;
+cout<<"enter your major:";
+ cin>> major;
+cout<<"enter your favroite movie:";
+ cin>> movie;
+cout<<"My name is "<<name << "My major is "<< major, << "My favroit movie is "<<movie; 
+
+return 0;
+
+}
